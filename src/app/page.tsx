0be84a06e9580a6d538f4e86e9d87f4647b07de2
Ranj <ranjdotdev@gmail.com>
2025-04-03 10:46:56 +0300

    Initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import type { JSX } from "react";
+
+export default function HomePage(): JSX.Element {
+  return (
+    <main className="bg-background text-foreground flex min-h-screen items-center justify-center">
+      nothing.
+    </main>
+  );
+}
---
+++ src/env.js
@@ -0,0 +1,22 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url().optional(),
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+  },
+  client: {
+    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  },
+
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: process.env.NODE_ENV,
+    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+  },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
+});
